6fce051bed1cbcf964aa143a9f59703ba84c339d
Jimmy Cleary <jimmehc@gmail.com>
2016-09-17 11:11:50 -0700

    Make it work with wrappers (way too slow though, will probably change radically)

--- LSColorize.psm1
+++ LSColorize.psm1
@@ -1,41 +1,263 @@
+$script:lsColors = @{}
 
-function Invoke-ColorizedLS
+function Set-LSColor
 {
-    Invoke-Expression "get-childitem $args" | Format-High Name -Print {    
-        $item = $args
-        $fore = $host.UI.RawUI.ForegroundColor        
-        $ANSICode = ""
-        $host.UI.RawUI.ForegroundColor = .{     
-            if ($item[1].psIsContainer) {'Blue'}
-#            elseif ($item[1].Extension -match '\.(exe|bat|cmd|ps1|psm1|vbs|rb|reg|dll|o|lib)') {'Green'}
-            elseif ($item[1].Extension -match '\.(zip|tar|gz|rar)') {'Yellow'}
-            elseif ($item[1].Extension -match '\.(py|pl|cs|rb|h|cpp|ps1)') {'Cyan'}
-            elseif ($item[1].Extension -match '\.(txt|cfg|conf|ini|csv|log|xml)') {'Red'}
-            else {$fore}
-        }
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0, Mandatory=$true)]
+        [ValidateScript({$script:lsColors.ContainsKey($_)})]
+        [string]$Name,
+        [Parameter(Position=1, Mandatory=$true)]
+        [ScriptBlock]$Condition,
+        [Parameter(Position=2, Mandatory=$true)]
+        [ConsoleColor]$Color
+    )
 
-        # Make code/executables/libraries green and bold, if ConEmu is set up right...
-        if ($item[1].Extension -match '\.(exe|bat|cmd|ps1|psm1|vbs|rb|reg|dll|o|lib)')
-        {
-            $ANSICode = "$([char]27)[32;42;1m";
-        }
+    $script:lsColors[$Name] = [pscustomobject]@{Color=$Color;Condition=$Condition}
+}
 
-        write-host "$ANSICode$($args[0])" -NoNewLine
-        $host.UI.RawUI.ForegroundColor = $fore
+function Add-LSColor
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0, Mandatory=$true)]
+        [string]$Name,
+        [Parameter(Position=1, Mandatory=$true)]
+        [ScriptBlock]$Condition,
+        [Parameter(Position=2, Mandatory=$true)]
+        [ConsoleColor]$Color
+    )
+
+    if($script:lsColors.ContainsKey($Name))
+    {
+        Write-Error "The ls color with name, $Name, already exists.  Use Set-LSColor to set it to a new value."
+        return
+    }
+
+    $script:lsColors[$Name] = [pscustomobject]@{Color=$Color;Condition=$Condition}
+}
+
+function Get-LSColor
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0, Mandatory=$false)]
+        [ValidateScript({$script:lsColors.ContainsKey($_)})]
+        [string]$Name
+    )
+
+    if(![string]::IsNullOrEmpty($Name))
+    {
+        $script:lsColors[$Name]
+    }
+    else
+    {
+        $script:lsColors
     }
 }
 
-function Invoke-ls 
+function script:Get-LSColorForFile
 {
-    # This could be better...
-	if($MyInvocation.PipelinePosition -ne $MyInvocation.PipelineLength)
-	{
-		iex "Get-ChildItem $args";
-	}
-	else
-	{
-		iex "Invoke-ColorizedLS $args";
-	}
+    param ($fileInfo)
+    $outColor = $host.UI.RawUI.ForegroundColor
+    foreach($key in $lsColors.Keys)
+    {
+        $cond = $lsColors[$key].Condition
+        $color = $lsColors[$key].Color
+        if(Foreach-Object -InputObject $fileInfo -Process $cond)
+        {
+            $outColor = $color
+            break
+        }
+    }
+    $outColor
+}
+
+Add-LSColor Dir {$_.PSIsContainer} Blue
+Add-LSColor Exec {$_.Extension -match '\.(exe|bat|cmd|ps1|psm1|vbs|rb|reg|dll|o|lib)'} Green
+Add-LSColor Archive {$_.Extension -match '\.(zip|tar|gz|rar)'} Yellow
+Add-LSColor Source {$_.Extension -match '\.(py|pl|cs|h|cpp)'} Cyan
+Add-LSColor Text {$_.Extension -match '\.(txt|cfg|conf|ini|csv|log|xml)'} Red
+
+function Get-ChildItemColored
+{
+    [CmdletBinding(DefaultParameterSetName='Items', SupportsTransactions=$true, HelpUri='http://go.microsoft.com/fwlink/?LinkID=113308')]
+    param(
+        [Parameter(ParameterSetName='Items', Position=0, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+        [string[]]
+        ${Path},
+
+        [Parameter(ParameterSetName='LiteralItems', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [Alias('PSPath')]
+        [string[]]
+        ${LiteralPath},
+
+        [Parameter(Position=1)]
+        [string]
+        ${Filter},
+
+        [string[]]
+        ${Include},
+
+        [string[]]
+        ${Exclude},
+
+        [Alias('s')]
+        [switch]
+        ${Recurse},
+
+        [uint32]
+        ${Depth},
+
+        [switch]
+        ${Force},
+
+        [switch]
+        ${Name}
+    )
+
+
+    dynamicparam
+    {
+        try {
+            $targetCmd = $ExecutionContext.InvokeCommand.GetCommand('Microsoft.PowerShell.Management\Get-ChildItem', [System.Management.Automation.CommandTypes]::Cmdlet, $PSBoundParameters)
+            $dynamicParams = @($targetCmd.Parameters.GetEnumerator() | Microsoft.PowerShell.Core\Where-Object { $_.Value.IsDynamic })
+            if ($dynamicParams.Length -gt 0)
+            {
+                $paramDictionary = [Management.Automation.RuntimeDefinedParameterDictionary]::new()
+                foreach ($param in $dynamicParams)
+                {
+                    $param = $param.Value
+
+                    if(-not $MyInvocation.MyCommand.Parameters.ContainsKey($param.Name))
+                    {
+                        $dynParam = [Management.Automation.RuntimeDefinedParameter]::new($param.Name, $param.ParameterType, $param.Attributes)
+                        $paramDictionary.Add($param.Name, $dynParam)
+                    }
+                }
+                return $paramDictionary
+            }
+        } catch {
+            throw
+        }
+    }
+
+    begin
+    {
+        try {
+            $outBuffer = $null
+            if ($PSBoundParameters.TryGetValue('OutBuffer', [ref]$outBuffer))
+            {
+                $PSBoundParameters['OutBuffer'] = 1
+            }
+            $wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand('Microsoft.PowerShell.Management\Get-ChildItem', [System.Management.Automation.CommandTypes]::Cmdlet)
+            $scriptCmd = { & $wrappedCmd @PSBoundParameters | %{ Add-Member -InputObject $_ @{"__Color"=(script:Get-LSColorForFile $_)} -PassThru } }
+            $steppablePipeline = $scriptCmd.GetSteppablePipeline($myInvocation.CommandOrigin)
+            $steppablePipeline.Begin($PSCmdlet)
+        } catch {
+            throw
+        }
+    }
+
+    process
+    {
+        try {
+            $steppablePipeline.Process($_)
+        } catch {
+            throw
+        }
+    }
+
+    end
+    {
+        try {
+            $steppablePipeline.End()
+        } catch {
+            throw
+        }
+    }
+    <#
+
+    .ForwardHelpTargetName Microsoft.PowerShell.Management\Get-ChildItem
+    .ForwardHelpCategory Cmdlet
+
+    #>
+}
+
+function global:Out-Default
+{
+    [CmdletBinding(HelpUri='http://go.microsoft.com/fwlink/?LinkID=113362', RemotingCapability='None')]
+    param(
+        [switch]
+        ${Transcript},
+
+        [Parameter(ValueFromPipeline=$true)]
+        [psobject]
+        ${InputObject})
+
+    begin
+    {
+        try {
+            $colorLSItems = New-Object System.Collections.ArrayList
+            $outBuffer = $null
+            if ($PSBoundParameters.TryGetValue('OutBuffer', [ref]$outBuffer))
+            {
+                $PSBoundParameters['OutBuffer'] = 1
+            }
+            $wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand('Microsoft.PowerShell.Core\Out-Default', [System.Management.Automation.CommandTypes]::Cmdlet)
+            $scriptCmd = {& $wrappedCmd @PSBoundParameters }
+            $steppablePipeline = $scriptCmd.GetSteppablePipeline($myInvocation.CommandOrigin)
+            $steppablePipeline.Begin($PSCmdlet)
+        } catch {
+            throw
+        }
+    }
+
+    process
+    {
+        try {
+            if($_.__Color)
+            {
+                $colorLSItems.Add($_) | Out-Null
+            }
+            else
+            {
+                $steppablePipeline.Process($_)
+            }
+        } catch {
+            throw
+        }
+    }
+
+    end
+    {
+        try {
+            if($colorLSItems.Length -gt 0)
+            {
+                 Format-High -InputObject $colorLSItems Name -Print {
+                    $fore = $host.UI.RawUI.ForegroundColor
+                    if($args[1].__Color)
+                    {
+                        $host.UI.RawUI.ForegroundColor = $args[1].__Color
+                    }
+                    write-host "$($args[0])" -NoNewLine
+                    $host.UI.RawUI.ForegroundColor = $fore
+                }
+            }
+            else
+            {
+                $steppablePipeline.End()
+            }
+        } catch {
+            throw
+        }
+    }
+    <#
+
+    .ForwardHelpTargetName Microsoft.PowerShell.Core\Out-Default
+    .ForwardHelpCategory Cmdlet
+
+    #>
 }
 
 if(Test-Path alias:ls)
@@ -43,7 +265,6 @@ if(Test-Path alias:ls)
     Write-Error "Alias 'ls' is defined.  Remove it with 'Remove-Item alias:ls -Force' before importing this module"
     return
 }
-Set-Alias ls Invoke-ls
-Set-Alias hi Invoke-ls
+Set-Alias ls Get-ChildItemColored
 
 Export-ModuleMember -Function * -Alias *
